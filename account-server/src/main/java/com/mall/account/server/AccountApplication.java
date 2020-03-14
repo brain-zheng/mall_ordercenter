f978cc6accd139f69d14b15b4e30173b13850396
郑海健 <brainhai0619@gmail.com>
2020-03-14 19:57:35 +0800

    initialization commit

---
+++ account-server/src/main/java/com/mall/account/server/AccountApplication.java
@@ -0,0 +1,18 @@
+package com.mall.account.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zheng haijain
+ * @createTime 2020-03-14 19:35
+ * @description
+ */
+@SpringBootApplication
+public class AccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+
+}
