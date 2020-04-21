af01eb6e9bf13d66f07be0dec4105bea7fe4c7b3
brainzheng <2209829964@qq.com>
2020-04-21 16:56:04 +0800

    first commit

---
+++ ordercenter-server/src/main/java/com/mall/ordercenter/server/OrderCenterApplication.java
@@ -0,0 +1,24 @@
+package com.mall.ordercenter.server;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * @author zheng haijain
+ * @createTime 2020-03-14 19:35
+ * @description
+ */
+@SpringBootApplication
+@MapperScan(value = "com.mall.account.dal.dao")
+@ComponentScan(value = "com.mall.account")
+@ImportResource(value = {"classpath:dubbo.xml"})
+public class AccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+
+}
